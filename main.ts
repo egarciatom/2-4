95a2a534dc70872c2356414086e81bf33c317bb8
egarciatom <101406426+egarciatom@users.noreply.github.com>
2022-03-25 14:21:29 +0100

    Actualizar main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,17 @@
 basic.forever(function () {
-	
+    if (input.lightLevel() < 100) {
+        basic.showNumber(5)
+    }
+    if (input.lightLevel() < 75) {
+        basic.showNumber(4)
+    }
+    if (input.lightLevel() < 55) {
+        basic.showNumber(3)
+    }
+    if (input.lightLevel() < 35) {
+        basic.showNumber(2)
+    }
+    if (input.lightLevel() < 20) {
+        basic.showNumber(1)
+    }
 })
